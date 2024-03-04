1ff480ac938e19cbcf2e3e0ae5dc85a603447694
Ahmed Nagy <dawood101dawood@gmail.com>
2024-03-04 10:06:26 +0200

    explain of testandset

--- TestAndSet.py
+++ TestAndSet.py
@@ -4,6 +4,8 @@ import random
 
 class TestAndSetLock:
     def __init__(self):
+        # self.lock: This attribute is initialized as a threading lock (threading.Lock()). 
+        #It is used to synchronize access to shared resources among multiple threads.
         self.lock = threading.Lock()
         self.flag = False
 
@@ -14,11 +16,15 @@ class TestAndSetLock:
             return old_flag
 
 class Process(threading.Thread):
+    #inheriting from threading.Thread means that instances of the Process class will behave as threads.
+    # This inheritance allows you to create threads easily by instantiating objects of the Process class 
+    #and starting them using the start() method provided by the Thread class.
     def __init__(self, process_id, lock):
         super(Process, self).__init__()
         self.process_id = process_id
         self.lock = lock
 
+    #By subclassing threading.Thread, you can define the behavior of the thread by overriding the run() method.
     def run(self):
         print(f"Process {self.process_id} trying to enter the critical section")
         while self.lock.test_and_set():
